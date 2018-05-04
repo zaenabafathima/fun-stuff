04e38ae6cb6c46e0e2a0d7ae982593c4b2929de8
Zaenab <zaenabafathima@gmail.com>
2018-05-04 17:11:39 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,17 +1,20 @@
-#something I've been wanting to do for a while...
+''' 
+The story behind the script:
+My VTU 8th sem results were due and I had recently procured a monitor 
+to look more hard-core at work like the other devs. 
+Leveraging both these facts, I wrote a script to keep polling the VTU website. 
+The result:
+1. The monitor had a terminal running with this script - looks cooler than a wallpaper
+2. I could check if my results were out (or not)
+'''
 import urllib
 import re
-# import requests
 from bs4 import BeautifulSoup
 from lxml import html
-# import operator
-# import time
 import datetime
 
 while(1):
-
-    usn = "179"
-
+    usn = "001"
     url1 = "http://results.vtu.ac.in/results17/result_page.php?usn=1pe13cs"
     page = urllib.urlopen(url1+usn)
     soup = BeautifulSoup(page, "lxml")
@@ -21,7 +24,7 @@ while(1):
 
     if notOut:
         print "Results not yet out...chill", datetime.datetime.now().time()
-        time.sleep(300)
+        time.sleep(60*5)  
 
     else:
         print "PANIC!!!!!----PANIC--------PANIC---------PANIC-----------PANIC-----------"

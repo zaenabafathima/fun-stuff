274fdc95995495dcb07c8fb253c548effdc63829
Zaenab <zaenabafathima@gmail.com>
2018-05-04 18:37:19 +0530

    Update and rename get.py to get_everyones_marks.py

--- get_everyones_marks.py
+++ get_everyones_marks.py
@@ -1,4 +1,8 @@
-#something I've been wanting to do for a while...
+'''
+After getting a panic alert from the previous script, this one will take the next step.
+It fetches everyone's marks and prints them in descending order
+A quick way to see who got more than me :P
+'''
 import urllib
 import re
 import requests
@@ -10,6 +14,7 @@ import csv
 usn = ""
 phoneBook = {}
 
+
 for slno in range(1,186+1):
     if slno < 10:
         usn = "00" + str(slno)
@@ -17,38 +22,27 @@ for slno in range(1,186+1):
         usn = "0"+str(slno)
     else:
         usn = str(slno)
-    # print usn
 
 
     url1 = "http://results.vtu.ac.in/reval_results17/result_page.php?usn=1pe13cs"
-    url2 = "http://results.vtu.ac.in/results/result_page.php?usn=1pe14cs"
-    page=urllib.urlopen(url1+usn)
-    soup=BeautifulSoup(page, "lxml")
-    textOnly=soup.get_text()
-    # print textOnly
-
+    page = urllib.urlopen(url1+usn)
+    soup = BeautifulSoup(page, "lxml")
+    textOnly = soup.get_text()
 
     name = re.search('(Student Name.[: \n\t\r]*)([A-Z \.]*)', textOnly)
 
 
     if name:
-        # print "Name:", name.group(2), "1PE13CS"+usn
-        marks=re.search('(Total Marks[\n]:[ ])([0-9]+)', textOnly)
+        marks = re.search('(Total Marks[\n]:[ ])([0-9]+)', textOnly)
         if marks:
             if name.group(2):
                 phoneBook[name.group(2)] = int(marks.group(2))
-		#phoneBook[name.group(2)] = usn
-                # print "Marks:", marks.group(2)
         else:
             print "Can't find marks"
     else:
         print "I don't exist"
 
 sorted_phoneBook = sorted(phoneBook.items(), key=operator.itemgetter(1))
-
-# with open('marksOfAll.csv', 'wb') as myfile:
-#     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-#     wr.writerow(sorted_phoneBook)
 
 for k in reversed(sorted_phoneBook):
     print k, (k[1]/750.0)*100

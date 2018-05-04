8e858988c768a4829a6148bc3bea22c123fee168
Zaenab <zaenabafathima@gmail.com>
2018-05-04 18:48:37 +0530

    Update and rename visualize.py to visualize_video_viewcount.py

--- visualize_video_viewcount.py
+++ visualize_video_viewcount.py
@@ -22,12 +22,15 @@ def process_file_data(fname):
 			time.append(record[0])
 			views.append(int(record[1]))  #converting to int!! Coz 1850000 is less than 75000 in the world of strings (learnt the hard way)
 
+''' I know, this should be in a loop, 
+but do understand the excitement of adding each entry every day!! '''
 process_file_data('video_stats.txt') #Feb 7
 process_file_data('video_stats2.txt') #Feb 8
 process_file_data('video_stats3.txt') #Feb 9
 process_file_data('video_stats4.txt') #Feb 12
 process_file_data('video_stats5.txt') #Feb 13
 process_file_data('video_stats6.txt') #Feb 14
+
 print("\n\nThe Time array===>", time)
 print("\nThe views array====>", views)
 
